7280967279da3e514f18ec82d134b106a0079ff0
Bryan Dellinger <dellingerfam5@gmail.com>
2022-08-31 13:40:28 -0400

    adding microsft mgt

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { Providers, ProviderState } from '@microsoft/mgt';
+import { Login } from '@microsoft/mgt-react'; // import login components
+import { Header, List } from 'semantic-ui-react';
+
+
+function useIsSignedIn(): [boolean] {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    }
+  }, []);
+
+  return [isSignedIn];
+}
 
 function App() {
+  const [isSignedIn] = useIsSignedIn();
+  const [activities, setActivities] = useState([]);
+
+  const getActivities = () => {
+    if (Providers.globalProvider.state === ProviderState.SignedIn) {
+      const provider = Providers.globalProvider;
+      let graphClient = provider.graph.client;
+      graphClient.api('/groups/88d59881-7b15-4adc-a756-5d10681cf99d/calendar/events').get()
+        .then(response => {
+          console.log(response);
+          setActivities(response.value)
+        })
+    }
+  }
+
+  const providerStateChanged = () => {
+    console.log('provider State Changed');
+    getActivities();
+  }
+
+  useEffect(() => {
+    console.log('use effect');
+    getActivities();
+  }, [])
+
+  Providers.onProviderUpdated(providerStateChanged);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h1' textAlign='right'>
+        <Login />
+      </Header>
+      {isSignedIn &&
+        <div>
+          <List>
+            {activities.map((activity: any) => (
+              <List.Item key={activity.id}>
+                {activity.subject}
+              </List.Item>
+            ))}
+          </List>
+        </div>
+      }
     </div>
   );
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: "bd7c447b-e640-4a8a-99b9-f31769b9ab66",
+  authority: "https://login.microsoftonline.com/0b23030e-3028-4f52-a8f1-a9898322c7fe",
+  redirectUri: "http://localhost:3000/",
+  scopes: ['calendars.read',
+           'user.read',
+           'user.read.all',
+           'openid',
+           'profile',
+          'people.read',
+          'user.readbasic.all',
+          'group.read.all'
+        ]
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
