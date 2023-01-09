768b144f97dd72d98ce5e2289a9f5521845fad16
Bryan Dellinger <bryan.dellinger.civ@armywarcollege.edu>
2023-01-09 11:26:10 -0500

    starting on hosting report

---
+++ API/Controllers/HostingReportsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.HostingReports;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Controllers
+{
+    public class HostingReportsController : BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> GetHostingReports() =>
+      HandleResult(await Mediator.Send(new List.Query()));
+    }
+}
